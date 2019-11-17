ba7a9e9eb5448efa6c333af9ad6035f0916f6755
Ulad Bohdan <vlbohdan@gmail.com>
2019-11-17 02:37:31 +0200

    three extra discount cards

--- index.js
+++ index.js
@@ -7,30 +7,27 @@ const TIMES = ["as soon as possible", "next week", "next month", "whenever"]
 export function App() {
   const [parkIndex, setParkIndex] = useState(0);
   const [timeIndex, setTimeIndex] = useState(1);
-  const [discountSet, setDiscounts] = useState([1, 2])
 
   const DISCOUNTS = {
-    '00': [1, 2],
-    '01': [1, 2],
-    '02': [1],
-    '03': [1],
-    '10': [2],
-    '11': [2],
-    '12': [],
-    '13': [],
+    '00': [2, 3, 4],
+    '01': [3, 4],
+    '02': [2, 4],
+    '03': [2, 4],
+    '10': [1, 2, 3, 5],
+    '11': [1, 2, 3, 5],
+    '12': [1, 5],
+    '13': [1, 2, 5],
   }
 
-  const updateCards = () => {
-    setDiscounts(DISCOUNTS[`${parkIndex % PARKS.length}${timeIndex % TIMES.length}`]);
-  }
+  const discountSet = DISCOUNTS[`${parkIndex % PARKS.length}${timeIndex % TIMES.length}`];
 
   return <>
     <div className="title">
       <span>Plan your trip to</span>
       {" "}
-      <span className="title__dropdown title__park" onClick={() => { setParkIndex((parkIndex + 1) % PARKS.length); updateCards(); }}>{ PARKS[parkIndex] }</span>
+      <span className="title__dropdown title__park" onClick={() => setParkIndex((parkIndex + 1) % PARKS.length)}>{ PARKS[parkIndex] }</span>
       {" "}
-      <span className="title__dropdown title__time" onClick={() => { setTimeIndex((timeIndex + 1) % TIMES.length); updateCards(); }}>{ TIMES[timeIndex] }</span>
+      <span className="title__dropdown title__time" onClick={() => setTimeIndex((timeIndex + 1) % TIMES.length)}>{ TIMES[timeIndex] }</span>
     </div>
     <div className="emojibuf">
       {"🌲 ⛰ 🇫🇮"}
@@ -46,18 +43,19 @@ export function App() {
 
       {discountSet.includes(1) && (<div className="card card-orange">
         <div className="card__title">
-          Mondays are awesome. Hotels🏨 are inviting
+          Thursdays are awesome. Hotels🏨 are inviting
         </div>
+        <img className="card__pic" src="https://res.cloudinary.com/dhgy4yket/image/upload/v1573946168/junc19/finland_parks_visitors_per_weekday.png" />
         <div className="card__subtitle">
-          Our data analysis revealed: mondays are extremely unpopular among the visitors.
-          <b>Hotelli Jeris</b> and <b>Aurora Dome & Glamping</b> share our concerns and provide
+          Our data analysis revealed: thursdays are the least popular among the visitors.
+          <b>Hotelli Jeris</b> and <b>Aurora Dome & Glamping</b> accepted the challenge and provide
           a <b>15% discount</b> for reservations through Visit Finland. Claim your discount below:
         </div>
         <div className="card__claim-button">
-          Book for Monday-Tuesday, Nov 25
+          Book for Thursday-Friday, Nov 21
         </div>
         <div className="card__claim-button">
-          Book for Monday-Tuesday, Dec 2
+          Book for Thursday-Friday, Dec 5
         </div>
       </div>)}
 
@@ -73,6 +71,49 @@ export function App() {
         </div>
         <div className="card__claim-button">
           Add to the calendar: sunny on Dec 3
+        </div>
+      </div>)}
+
+      {discountSet.includes(3) && (<div className="card card-blue">
+        <div className="card__title">
+          Daylength is rapidly shortening🌅 Around 4.5 minutes a day in South Finland. 8.5 minutes in Lapland
+        </div>
+        <div className="card__subtitle">
+          Of course you'd prefer to go while it's not completely dark! Don't miss it.
+        </div>
+        <div className="card__claim-button">
+          Lapland sun will be down all day from Dec 11
+        </div>
+        <div className="card__claim-button">
+          South Finland will be lightened less than 6 hours a day on Dec 21
+        </div>
+      </div>)}
+
+      {discountSet.includes(4) && (<div className="card card-grey">
+        <div className="card__title">
+          Avoid crowds! Weekly stats for trails in Nuuksio
+        </div>
+        <img className="card__pic" src="https://res.cloudinary.com/dhgy4yket/image/upload/v1573950596/junc19/north.png" />
+        <div className="card__subtitle">
+          Thanks to the counters, we have years of historical data for different trails within the Park.
+          Research the map and avoid using the crowdiest paths
+        </div>
+        <div className="card__claim-button">
+          Nuuksio map, week ago
+        </div>
+      </div>)}
+
+      {discountSet.includes(5) && (<div className="card card-green">
+        <div className="card__title">
+          Pallas-Yllästunturi historical stats. Avoid crowdy trails!
+        </div>
+        <img className="card__pic" src="https://res.cloudinary.com/dhgy4yket/image/upload/v1573950596/junc19/south.png" />
+        <div className="card__subtitle">
+          Thanks to the counters, we have years of historical data for different trails within the Park.
+          Research the map and avoid using the crowdiest paths
+        </div>
+        <div className="card__claim-button">
+          Pallas-Yllästunturi trails on Maps
         </div>
       </div>)}
 
